9bc910e335f831569eaceaab604c0c0e21326f7d
andreevmo <maxiandreev1994@gmail.com>
2022-10-02 13:25:18 +0400

    Ready to deploy

--- java-web-ru/deploy/src/main/java/exercise/servlet/CompaniesServlet.java
+++ java-web-ru/deploy/src/main/java/exercise/servlet/CompaniesServlet.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.util.*;
 import java.util.stream.Collectors;
 import static exercise.Data.getCompanies;
 
